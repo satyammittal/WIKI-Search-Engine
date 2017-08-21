56b7bfd33ca51f8c580cf531177a81ccd5fbc3ed
Satyam Mittal <satyammittalid@gmail.com>
2017-08-21 19:18:57 +0530

    updated

--- indexer.java
+++ indexer.java
@@ -20,18 +20,18 @@ public class indexer {
    public static HashMap<String,Integer> tf=new HashMap<String,Integer>();
    public static HashMap<String,Integer> idf=new HashMap<String,Integer>(); 
    public static HashMap<String,Integer> mat=new HashMap<String,Integer>(); 
-   public static TreeMap<String, TreeMap<Integer, List<Pair<Integer, String>>>> indexer;
-   private StringBuffer bodyText = new StringBuffer("");
+   public static TreeMap<String, List<Pair<Integer, String>>> indexer;
+   public static StringBuffer bodyText = new StringBuffer("");;
    public static void main(String[] args) {
 
      try {
          Instant start = Instant.now();
          File inputFile = new File("wiki-search-small.xml");
          SAXParserFactory factory = SAXParserFactory.newInstance();
-         indexer = new TreeMap<String, TreeMap<Integer, List<Pair<Integer, String>>>>();
+         indexer = new TreeMap<String, List<Pair<Integer, String>>>();
          SAXParser saxParser = factory.newSAXParser();
          UserHandler userhandler = new UserHandler();
-         userhandler.sample(tf, idf, indexer);
+         userhandler.sample(tf, idf, indexer, bodyText);
          saxParser.parse(inputFile, userhandler);    
          Map<String,Integer> idf_sorted = sortByValue(idf);
          Map<String,Integer> tf_sorted = sortByValue(tf);
@@ -42,40 +42,40 @@ public class indexer {
             mat.put((String)m.getKey(),s3);
          }  
       Map<String,Integer> mat_sorted = sortByValue(mat);
-      File statText = new File("indexfile3");
+      File statText = new File("indexfile5");
       FileOutputStream is = new FileOutputStream(statText);
       OutputStreamWriter osw = new OutputStreamWriter(is);    
       Writer w = new BufferedWriter(osw);
 
       //for(Map.Entry m:mat_sorted.entrySet()){  
        //
-                  //System.out.println(m.getKey()+" "+m.getValue());  
+                  ////System.out.println(m.getKey()+" "+m.getValue());  
       //}
 
-      for(Map.Entry<String, TreeMap<Integer, List<Pair<Integer, String>>>> entry : indexer.entrySet()) {
+      for(Map.Entry<String, List<Pair<Integer, String>>> entry : indexer.entrySet()) {
          String key = entry.getKey();
-         TreeMap<Integer, List<Pair<Integer, String>>> value = entry.getValue();
+         List<Pair<Integer, String>> pr2 = entry.getValue();
          w.write(key + "|");
-         for(Map.Entry<Integer, List<Pair<Integer, String>>> entry2 : value.entrySet()) 
-         {
-            Integer doc = entry2.getKey();
-            w.write(doc.toString() + ":");
-            List<Pair<Integer, String>> pr2 = entry2.getValue();
+         //for(Map.Entry<Integer, List<Pair<Integer, String>>> entry2 : value.entrySet()) 
+         //{
+            //Integer doc = entry2.getKey();
+            //w.write(doc.toString() + ":");
+           // List<Pair<Integer, String>> pr2 = entry.getValue();
             for (Pair<Integer, String> pr:pr2)
             {
                Integer in = pr.getFirst();
                String qr = pr.getSecond();
-               w.write(in.toString() + "$" + qr + ","); 
+               w.write(in.toString()+","); 
             }
-             w.write(";"); 
-         }
+          //   w.write(";"); 
+         //}
                w.write("\n"); 
       }
       w.close();
       Instant end = Instant.now();
       Duration timeElapsed = Duration.between(start, end);
       System.out.println("Time taken: "+ timeElapsed.toMillis() +" milliseconds");
-      //System.out.println(indexer);  
+      ////System.out.println(indexer);  
       } catch (Exception e) {
          e.printStackTrace();
       }
@@ -116,18 +116,22 @@ class UserHandler extends DefaultHandler {
    HashSet<String> set=new HashSet<String>();  
    HashMap<String,Integer> tf;
    HashMap<String,Integer> idf;
-   TreeMap<String, TreeMap<Integer, List<Pair<Integer, String>>>> index;
+   StringBuffer buffer;
+   TreeMap<String, List<Pair<Integer, String>>> index;
    Stemmer stemf;
    Integer id=-1;
    Integer numberword = 0;
    GenerateStopwords stopw = new GenerateStopwords();
    int counter=1;
-   public void sample(HashMap<String,Integer> tf,HashMap<String,Integer> idf,TreeMap<String, TreeMap<Integer, List<Pair<Integer, String>>>> indexer){
+   public void sample(HashMap<String,Integer> tf,HashMap<String,Integer> idf,TreeMap<String, List<Pair<Integer, String>>> indexer, StringBuffer bfr){
       this.tf=tf;
       this.idf=idf;
       this.index=indexer;
+      this.buffer=bfr;
       stemf = new Stemmer();
       stopw.generatefromfile("english.txt");
+      System.out.println("hello");
+
    }
    @Override
    public void startElement(String uri, 
@@ -137,18 +141,20 @@ class UserHandler extends DefaultHandler {
             // TODO: Whatever should happen when condition is reached
          return;
         }
-   //      //System.out.println("Roll No : " + qName);
+   //      ////System.out.println("Roll No : " + qName);
       if (qName.equalsIgnoreCase("revision")) {
          isRevision = true;
       } else if (qName.equalsIgnoreCase("title")) {
          isTitle = true;
       } else if (qName.equalsIgnoreCase("text")) {
+           buffer = new StringBuffer();
+       
          isBody = true;
          numberword = 0;
       }
       else if (qName.equalsIgnoreCase("id")) {
       //   String rollNo = attributes.getValue("id");
-       //  //System.out.println("Roll No : " + rollNo);
+       //  ////System.out.println("Roll No : " + rollNo);
          isId = true;
       }
    }
@@ -172,20 +178,9 @@ class UserHandler extends DefaultHandler {
       else if(tag.equalsIgnoreCase("text"))
       {
          isBody = false;
-         //System.out.println(String.valueOf(id));
-      }
-   }
-
-   @Override
-   public void characters(char ch[], int start, int length) throws SAXException {
-      set=new HashSet<String>();
-      if (isTitle) {
-       //  //System.out.println("Title: " 
-         //   + new String(ch, start, length));
-      } else if (isBody) {
-         String s2 = new String(ch, start, length);
+         String s2 = buffer.toString();
          s2=s2.toLowerCase();
-        // System.out.println(s2);
+        // //System.out.println(s2);
          String PATTERN_TOKEN = "\\$%{}[]()`<>='&:,;/.~ ;*\n|\"^_-+!?#\t@";
          //String s1 = s2.replaceAll("[-:()/^[!|=,?._'{}@+\\[\\]]]", " ");
          List<String> parts2 = new ArrayList<String>();
@@ -193,7 +188,7 @@ class UserHandler extends DefaultHandler {
             while(normalTokenizer.hasMoreTokens()){
                parts2.add(normalTokenizer.nextToken().trim());
             }
-        System.out.println(parts2);
+        //System.out.println(parts2);
         // String[] parts2 = s1.split("\\s+");
          Pattern rp = Pattern.compile("^[a-zA-Z0-9]*$");
          List<String> parts = new ArrayList<String>();
@@ -203,58 +198,58 @@ class UserHandler extends DefaultHandler {
          if (m.find())
             parts.add(p);
          }
-         //System.out.println(parts.toString());
+         ////System.out.println(parts.toString());
          for(String p:parts){
             String p2=p;
             stemf.add(p.toCharArray(), p.length());
             stemf.stem();
             p = stemf.toString();
-          System.out.println(p+ " ");
-          
-            if(p.length()>0 && !stopw.checkStopWord(p) && !stopw.checkStopWord(p2))
+            
+            if(p.length()>0 && !stopw.checkStopWord(p) && !stopw.checkStopWord(p2) && p.charAt(0)!='0')
             {
+              //System.out.println(p+ " ");
                if(!index.containsKey(p)) 
             {
-                  TreeMap<Integer, List<Pair<Integer, String>>> f = new TreeMap<Integer, List<Pair<Integer, String>>>();  
-                  Pair<Integer,String> r = new Pair<Integer,String>(numberword,"B");
+                  Pair<Integer,String> r = new Pair<Integer,String>(id,"B");
                   List<Pair<Integer,String>> ls = new LinkedList<Pair<Integer,String>>();
                   ls.add(r);
-                  f.put(id,ls);
-                  index.put(p,f);
+                 // index.put(id,ls);
+                  index.put(p,ls);
                }
                else{
-                  TreeMap<Integer, List<Pair<Integer, String>>> f = index.get(p);
-                  if(!f.containsKey(id))
-                  {
-                     Pair<Integer,String> r = new Pair<Integer,String>(numberword,"B");
-                     List<Pair<Integer,String>> ls = new LinkedList<Pair<Integer,String>>();
-                     ls.add(r);
-                     f.put(id,ls);
-                     index.put(p,f);
-                  }
-                  else{
-                     List<Pair<Integer,String>> ls = f.get(id);
+              
+                     List<Pair<Integer,String>> ls = index.get(p);
                      Pair<Integer,String> r = new Pair<Integer,String>(numberword,"B");
                      
                      ls.add(r);
-                     f.put(id,ls);
-                     index.put(p,f);
+                  //   f.put(id,ls);
+                     index.put(p,ls);
                   }
                }
                 numberword++;
                    
             }
-   
-            
-        }
+         ////System.out.println(String.valueOf(id));
+      }
+   }
 
+   @Override
+   public void characters(char ch[], int start, int length) throws SAXException {
+      set=new HashSet<String>();
+      if (isTitle) {
+       //  ////System.out.println("Title: " 
+         //   + new String(ch, start, length));
+      } else if (isBody) {
+          String s2 = new String(ch, start, length);
+         buffer.append(s2);
+         
       } else if (isRevision) {
-     //    //System.out.println("Revision: " + new String(ch, start, length));
+     //    ////System.out.println("Revision: " + new String(ch, start, length));
       } else if (isId && !isRevision) {
           String s2 = new String(ch, start, length);
          s2=s2.toLowerCase();
          id=Integer.valueOf(s2);
-         ////System.out.println(s2);
+         //////System.out.println(s2);
         
       }
    }
